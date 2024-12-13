2702b587f01f6fc50a474655914840635308c319
unknown <luutam_viva@yahoo.com>
2024-12-13 12:02:10 +0100

    aop Exo Book

--- AOP/LibraryAOP/src/main/java/org/example/libraryaop/aspect/ExceptionAspect.java
+++ AOP/LibraryAOP/src/main/java/org/example/libraryaop/aspect/ExceptionAspect.java
@@ -6,8 +6,10 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class ExceptionAspect {
     private  static Logger logger = LoggerFactory.getLogger(ExceptionAspect.class.getName());
 
--- AOP/LibraryAOP/src/main/java/org/example/libraryaop/aspect/PerformanceAspect.java
+++ AOP/LibraryAOP/src/main/java/org/example/libraryaop/aspect/PerformanceAspect.java
@@ -6,8 +6,10 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class PerformanceAspect {
 
     private static final Logger logger = LoggerFactory.getLogger(PerformanceAspect.class);
